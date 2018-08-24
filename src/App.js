724cc60b3792e683d6bbe400084d94afcf4cfb26
Marina Radeva <marina_radeva@yahoo.com>
2018-08-24 13:49:13 +0300

    Add Search component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BooksList from './BooksList'
+import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
-    books: []
+    books: [],
+    status: none
   }
-    showSearchPage: false
+
+
+    handleChange = (book, shelf) => {
+      BooksAPI.update(book, shelf).then(() => {
+        book.shelf = shelf
+        this.setState(previousState => ({
+          books: previousState.books.filter(shelf=> shelf.id !== book.id).concat([book])
+        }))
+        this.setState({status: book.shelf})
+      })
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
